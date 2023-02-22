c4efe71b59db35be5e026fa30def3fd6027d119e
isaacg <iwanyoike65@gmail.com>
2023-02-22 17:35:13 +0300

    check linter errors

--- src/index.js
+++ src/index.js
@@ -4,25 +4,22 @@ const tasks = [
   { description: 'Walk the dog', completed: false, index: 0 },
   { description: 'Buy groceries', completed: false, index: 1 },
   { description: 'Do laundry', completed: false, index: 2 },
-  { description: 'Call mom', completed: false, index: 3 }
-]
+  { description: 'Call mom', completed: false, index: 3 },
+];
 
 const divtasks = document.querySelector('.tasks');
 
 const addTask = (task) => {
-    const div = document.createElement('div');
-    div.classList.add('task');
-    div.innerHTML = `<div class = "task-check">
+  const div = document.createElement('div');
+  div.classList.add('task');
+  div.innerHTML = `<div class = "task-check">
                     <input type="checkbox" id="${task.index}" ${task.completed ? 'checked' : ''}>
                     <label for="${task.index}">${task.description}</label>
                     </div>
                     <span class="material-symbols-outlined">more_vert</span>`;
-    divtasks.appendChild(div);  
-    }
+  divtasks.appendChild(div);
+};
 
 tasks.forEach((task) => {
   addTask(task);
-}
-)
-// div.innerHTML = `<input type="checkbox" name="task" id="task-${task.index}" ${task.completed ? 'checked' : ''}>
-//     <label for="task-${task.index}">${task.description}</label>`;
+});
